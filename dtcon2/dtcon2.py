794ce5830fd4896484b3dcfa073a96de5a9d7f59
Philipp Roebrock <philipp.roebrock@gmail.com>
2011-07-15 12:47:02 +0200

    changed: implementation of check and print on same code base

--- dtcon2/dtcon2.py
+++ dtcon2/dtcon2.py
@@ -13,7 +13,6 @@ def LogPrint(lvl, message):
 		sys.exit()
 	else:
 		raise Exception('Unknown log level {0:d}'.format(lvl))
-		
 
 def CreateTables(dbcon):
 	dbcon.execute('create table nodes (' + \
@@ -27,7 +26,11 @@ def DropTables(dbcon):
 	dbcon.execute('drop table nodes')
 
 def ClearDB(dbcon):
-	dbcon.execute('delete from nodes')
+	# TODO: accept name here
+	# delete all nodes except the root nodes
+	dbcon.execute('delete from nodes where parent not null')
+	# clear all existing checksums for root nodes
+	dbcon.execute('update nodes set checksum=null where parent is null')
 
 def GetFileChecksum(path):
 	checksum = hashlib.sha256()
@@ -56,44 +59,96 @@ def CheckDirChecksum(path, checksum):
 	if checksum != GetDirChecksum(path):
 		LogPrint(1, 'Checksum error for directory ' + path)
 
-def Check(dbcon, rowid, path, isdir, checksum):
-	print('checking ' + path + ' ...')
-	if isdir:
-		if not os.path.isdir(path):
-			LogPrint(1, 'There is no directory ' + path + ', so we are ignoring it')
-			return
-		CheckDirChecksum(path, checksum)
-		childnodes = dbcon.cursor()
-		childnodes.execute('select rowid,path,isdir,checksum from nodes where parent=?', (rowid,))
-		for row in childnodes:
-			Check(dbcon, row[0], os.path.join(path,row[1]), row[2], row[3])
-		childnodes.close()
+def Import(dbcon, path):
+	print('importing ' + path + ' ...')
+	# TODO: check if path already exists as a root node
+	cur = dbcon.cursor()
+	if os.path.isdir(path):
+		cur.execute('insert into nodes (parent, path, isdir, checksum) values(null,?,?,?)', \
+			(path, True, GetDirChecksum(path)))
+		ImportRecurse(dbcon, cur.lastrowid, path)
+	elif os.path.isfile(path):
+		cur.execute('insert into nodes (parent, path, isdir, checksum) values(null,?,?,?)', \
+			(path, False, GetFileChecksum(path)))
 	else:
-		if not os.path.isfile(path):
-			LogPrint(1, 'There is no file ' + path + ', so we are ignoring it')
-			return
-		CheckFileChecksum(path, checksum)
+		LogPrint(2, 'Unknown directory entry ' + path + '.')
+	cur.close()
 
-def DoWithOneRoot(dbcon, path, func):
+def ImportRecurse(dbcon, rowid, path):
+	print('importing ' + path + ' ...')
 	cur = dbcon.cursor()
-	cur.execute('select rowid,path,isdir,checksum from nodes where parent is null and path=?', (path,))
-	row = cur.fetchone()
-	func(dbcon, row[0], row[1], row[2], row[3])
+	entries = os.listdir(path)
+	for e in entries:
+		fullpath = os.path.join(path, e)
+		if os.path.isdir(fullpath):
+			cur.execute('insert into nodes (parent, path, isdir, checksum) values(?,?,?,?)', \
+				(rowid, e, True, GetDirChecksum(fullpath)))
+			ImportRecurse(dbcon, cur.lastrowid, fullpath)
+		elif os.path.isfile(fullpath):
+			cur.execute('insert into nodes (parent, path, isdir, checksum) values(?,?,?,?)', \
+				(rowid, e, False, GetFileChecksum(fullpath)))
+		else:
+			LogPrint(2, 'Unknown directory entry.')
 	cur.close()
-	
-def DoWithAllRoots(dbcon, func):
+
+def ExecuteOnAllNodes(dbcon, path, rootfunc, innernodefunc):
 	cur = dbcon.cursor()
-	cur.execute('select rowid,path,isdir,checksum from nodes where parent is null')
+	if path == None:
+		cur.execute('select rowid,path,isdir,checksum from nodes where parent is null')
+	else:
+		cur.execute('select rowid,path,isdir,checksum from nodes where parent is null and path=?', (path,))
 	for row in cur:
-		func(dbcon, row[0], row[1], row[2], row[3])
+		if row[2]:
+			if not os.path.isdir(row[1]):
+				LogPrint(1, 'There is no directory ' + row[1] + ', so we are ignoring it')
+				return
+			rootfunc(dbcon, row[0], None, row[1], row[1], row[2], row[3])
+			ExecuteOnAllRecurse(dbcon, row[0], row[1], innernodefunc)
+		else:
+			if not os.path.isfile(row[1]):
+				LogPrint(1, 'There is no file ' + row[1] + ', so we are ignoring it')
+				return
+			rootfunc(dbcon, row[0], None, row[1], row[1], row[2], row[3])
 	cur.close()
+
+def ExecuteOnAllRecurse(dbcon, rowid, path, innernodefunc):
+	cur = dbcon.cursor()
+	cur.execute('select rowid,path,isdir,checksum from nodes where parent=?', (rowid,))
+	for row in cur:
+		fullpath = os.path.join(path, row[1]);
+		if row[2]:
+			if not os.path.isdir(fullpath):
+				LogPrint(1, 'There is no directory ' + fullpath + ', so we are ignoring it')
+				return
+			innernodefunc(dbcon, rowid, row[0], row[1], fullpath, row[2], row[3])
+			ExecuteOnAllRecurse(dbcon, row[0], fullpath, innernodefunc)
+		else:
+			if not os.path.isfile(fullpath):
+				LogPrint(1, 'There is no file ' + fullpath + ', so we are ignoring it')
+				return
+			innernodefunc(dbcon, rowid, row[0], row[1], fullpath, row[2], row[3])
+	cur.close()
+
+def PrintRoot(dbcon, rowid, parent, path, fullpath, isdir, checksum):
+	print('### root node (id {0:d}): {1:s}'.format(rowid, fullpath))
+
+def PrintInnerNode(dbcon, rowid, parent, path, fullpath, isdir, checksum):
+	print('node (id {0:d}, parent {1:d}, isdir {2:b}): {3:s}'.format(rowid, parent, isdir, fullpath))
+
+def CheckNode(dbcon, rowid, parent, path, fullpath, isdir, checksum):
+	print('checking ' + fullpath + ' ...');
+	if isdir:
+		CheckDirChecksum(fullpath, checksum)
+	else:
+		CheckFileChecksum(fullpath, checksum)
 
 
 
 dbcon = sqlite3.connect(':memory:')
 CreateTables(dbcon)
-dbcon.execute('insert into nodes values (null, "C:\\Projects\\src\\Mathlib", 1, "9c5c210cf46dab4dbe6599fdcf48d704aa8509bede853467bb794dbe9324a92e")')
-dbcon.execute('insert into nodes values (1,    "MathLib.sln",                0, "f051d99dcfe20aa882eb4821860b3bf0b5073950161580e8d0e56e73eb1d6290")')
-DoWithAllRoots(dbcon, Check)
+Import(dbcon, "C:\\Projects\\src\\Mathlib")
+Import(dbcon, "C:\\Projects\\Others\\dtcon2\\checkformat.py")
+#ExecuteOnAllNodes(dbcon, None, PrintRoot, PrintInnerNode)
+ExecuteOnAllNodes(dbcon, None, CheckNode, CheckNode)
 dbcon.close()
 
