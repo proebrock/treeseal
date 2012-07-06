84abeb9b23199bbcc675083e4ede0c03ddb09fc8
Philipp Roebrock <philipp.roebrock@gmail.com>
2012-07-06 21:03:04 +0200

    added some basic menu

--- gui_study.py
+++ gui_study.py
@@ -7,7 +7,25 @@ import wx
 class MainWindow(wx.Frame):
 	def __init__(self, parent):
 		wx.Frame.__init__(self, parent, title='dtint', size=(800,600))
+		
+		self.CreateStatusBar()
+		
+		fileMenu = wx.Menu()
+		#fileMenu.AppendSeparator()
+		menuExit = fileMenu.Append(wx.ID_EXIT, 'E&xit', 'Terminate Program')
+		self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
+		helpMenu = wx.Menu()
+		menuAbout = helpMenu.Append(wx.ID_ABOUT, 'About', 'Information about this program')
+		# assemble menu
+		menuBar = wx.MenuBar()
+		menuBar.Append(fileMenu, '&File')
+		menuBar.Append(helpMenu, 'Help')
+		self.SetMenuBar(menuBar)
+		
 		self.Show(True)
+		
+	def OnExit(self, e):
+		self.Close(True)
 		
 app = wx.App(False)
 frame = MainWindow(None)
