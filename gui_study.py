4df5f5c06ab732bf30d7d2d28eb70092e7c499bf
Philipp Roebrock <philipp.roebrock@gmail.com>
2012-07-13 16:34:37 +0200

    add listctrl control for node list

--- gui_study.py
+++ gui_study.py
@@ -5,6 +5,9 @@ import wx
 
 
 class MainWindow(wx.Frame):
+	"""
+	Constructor of MainWindow
+	"""
 	def __init__(self, parent):
 		wx.Frame.__init__(self, parent, title='dtint', size=(800,600))
 		
@@ -24,18 +27,38 @@ class MainWindow(wx.Frame):
 		# main window consists of address line and directory listing
 		self.address = wx.TextCtrl(self, -1, style=wx.TE_READONLY)
 		self.address.SetValue('/home/phil/Data');
-		self.listing = wx.TextCtrl(self, -1, 'Listbox (TODO)', style=wx.TE_READONLY)
+		self.list = wx.ListCtrl(self, style=wx.LC_REPORT)
+		self.list.InsertColumn(0, 'Status')
+		self.list.InsertColumn(1, 'Name')
+		self.list.InsertColumn(2, 'IsDir')
+		self.list.InsertColumn(3, 'Size')
+		self.list.InsertColumn(4, 'CTime')
+		self.list.InsertColumn(5, 'ATime')
+		self.list.InsertColumn(6, 'MTime')
+		self.list.InsertColumn(7, 'Checksum')
+
+		self.list.InsertStringItem(0, 'OK')
+		self.list.SetStringItem(0, 1, 'result.m')
+		self.list.SetStringItem(0, 2, 'False')
+		self.list.SetStringItem(0, 3, '23KB')
+		self.list.SetStringItem(0, 4, '2012-05-06 10:23:11')
+		self.list.SetStringItem(0, 5, '2012-05-06 10:23:12')
+		self.list.SetStringItem(0, 6, '2012-05-06 10:23:13')
+		self.list.SetStringItem(0, 7, '7c6a6523ac1238a9...')
 		
 		box = wx.BoxSizer(wx.VERTICAL)
 		box.Add(self.address, 0, wx.EXPAND)
 		box.AddSpacer((5,0))
-		box.Add(self.listing, 1, wx.EXPAND)
+		box.Add(self.list, 1, wx.EXPAND)
 		self.SetSizer(box)
 		
 		self.CreateStatusBar()
 		
 		self.Show(True)
-		
+
+	"""
+	Executed on program exit
+	"""
 	def OnExit(self, e):
 		self.Close(True)
 		
